cefdf35c05c527da4b38ca23092861947a13ed0d
JohnMMcD <jmcdonne+github@gmail.com>
2023-03-06 17:27:35 -0500

    Now writes files instead of stdout. Requires one parameter.

--- README.md
+++ README.md
@@ -3,49 +3,38 @@ This generates files which can help judges at events. It is not supported by or 
 
 # Prerequisites
 These instructions assume:
-* some familiarity with programming software in Java and operating an IDE
 * some familiarity with the FIRST Tech Challenge scoring system
+* some familiarity with programming software in Java and operating an IDE (the author used IntelliJ)
+* some familiarity with importing delimited data into spreadsheet programs
 
 # Setup Instructions
 Clone the repository and open with your IDE (the author used IntelliJ). Configure your IDE to compile using Java 8 or higher.
 
-If you are proficient using the JDK, you can copy the `src/main/java/Connect.java` file, add a sqlite driver to your classpath, and compile it.
+If you are proficient using the JDK, you can copy the `src/main/java/Connect.java` file, add a sqlite driver to the current directory, compile the class, then run it with:
+    java -cp sqlite.jar:. JudgeHelper DATABASE_FILE_INCLUDING_EXTENSION
 
 Well before the event, you should download the FIRST Tech Challenge scoring system locally, do a manual event setup 
 with fake teams in offline mode, and go through the steps below to make sure you can do them correctly on the day of the event.
 
 # Day of Event Instructions
 * In the FIRST Tech Challenge Scoring system, import the teams and follow the normal steps to generate the match list.
-* After the match list has been generated, export the database using the "Export Divisions" button. You can also copy the database files to a known location.
-* Change the `url` in `JudgeHelper.java` to match the location of the database file or files.
 * Optionally, customize the `header` to what your Judge Match Observers want to evaluate.
-* Compile and execute `JudgeHelper.java` from within your IDE, redirecting the output to a file. Executing the JudgeHelper file will generate two sections, separated by some blank lines, which should look like the sample output at the bottom of this readme.
-* Copy each section into its own file.
-* For each section / file, open a new Excel workbook and use the 'Data' area of the Ribbon to import the file. Save each file in Excel format.
+* After the match list has been generated, export the database using the "Export Divisions" button.
+* Copy the database files to a known location, such as the same directory as the cloned repository.
+* Add a command-line parameter with the name of the database file.
+* Compile and execute `JudgeHelper.java` from within your IDE. 
+* Executing the JudgeHelper file will generate several files. 
+* For each file (see below for what file does what):
+  * open a new spreadsheet
+  * Excel users should use the 'Data' area of the Ribbon to import the file. Mac users, launch Numbers, then drag the file from Finder onto the Numbers icon in the Dock.
+  * Save the file in the native spreadsheet format.
+  * For '*paper* files, adjust the column and row sizes, set printing area, and print the files.
  
-The first section can be used by your Judge Match Observers to note what the teams are doing on the field. The Judge Match Observers should use the Filter function in Excel to show only the current match, and change it after each match. Remind them to save the file every once in a while.
+The jmo_* files can be used by your Judge Match Observers to note what the teams are doing on the field. The '*paper' files
+are designed to be printed, while the '*spreadsheet*' files are designed for direct data entry.
 
-The second section shows the matches which each team will be playing in; this can be used by judges doing pit interviews to help them determine the best time to interview a team.
+For direct data entry, the Judge Match Observers should use the Filter function in the spreadsheet to show only 
+the current match, and change the Filter after each match. Remind them to save the file every once in a while.
 
-    Team	Match	Navigated	Auto Cones	Game Play	Reliability	Standout
-    207 	 3 
-    207 	 7 
-    207 	 11 
-    207 	 15 
-    207 	 17 
-    237 	 1 
-    237 	 6 
-    237 	 10 
-    237 	 15 
-    237 	 19 
-    247 	 4 
-    247 	 6 
-    247 	 9 
-    247 	 12 
-    ...
-    207 	 Critical Mass 	 3 	 7 	 11 	 15 	 17 
-    237 	 MFS Foxes 	 1 	 6 	 10 	 15 	 19 
-    247 	 Reboot 	 4 	 6 	 9 	 12 	 16 
-    248 	 Fatal Error 	 4 	 7 	 10 	 13 	 17 
-    251 	 Mech-alodons 	 1 	 8 	 10 	 12 	 17 
-    ...
+The 'pitfinder' file shows the matches which each team will be playing in; this can be used by judges doing pit 
+interviews to help them determine the best time to interview a team.
--- src/main/java/JudgeHelper.java
+++ src/main/java/JudgeHelper.java
@@ -1,14 +1,75 @@
+import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /** See README.md for instructions. */
 public class JudgeHelper {
     // Alternate version for Windows:
     // static String url = "jdbc:sqlite:C:/ftc.db";
-    static String url = "jdbc:sqlite:/Users/johnmcdonnell/FTC/Scorekeeper_Binary/FIRST-Tech-Challenge-Live-v4.2.6/db/usnjcmptpke_1.db";
-    static String header = "Team\tMatch\tNavigated\tAuto Cones\tGame Play\tReliability\tStandout";
 
-    public static void showMatches(Connection con, Boolean makeTable) throws SQLException {
+    static String header_jmo = "Team\tMatch\tNavigated\tAuto Cones\tGame Play\tReliability\tStandout" + System.lineSeparator();
+    static String header_pit = "Team\tName\tM1\tM2\tM3\tM4\tM5\tM6\tM7\tM8" + System.lineSeparator();
+    static String red = "red";
+    static String blue = "blue";
+    static String both = "both";
+
+    public static void main(String[] args) {
+        if (args == null || args.length < 1 || args[0].trim().equals("")) {
+            System.err.println("Usage: java -cp sqlite.jar:. JudgeHelper DATABASE_FILE_INCLUDING_EXTENSION");
+            System.exit(1);
+        }
+        String us = null;
+        try {
+            File f = new File(args[0]);
+            us = f.getAbsolutePath();
+            System.out.println("Getting data from database file: " + us);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        // String url = "jdbc:sqlite:/Users/johnmcdonnell/FTC/Scorekeeper_Binary/FIRST-Tech-Challenge-Live-v4.2.6/db/usnjcmptpke_1.db";
+        String url = "jdbc:sqlite:" + us;
+
+        try {
+            Connection con = DriverManager.getConnection(url);
+            String pitFinder = showMatchesForSpreadsheet(con, false, both);
+            writeStringToFile(pitFinder, "pitfinder_spreadsheet.tsv");
+
+            // showMatchListRegular(con);
+
+            ArrayList<String> colors = new ArrayList<>(Arrays.asList(both, red, blue));
+
+            for (String color: colors) {
+                String matchesForPaper = showMatchesForPaper(con, color);
+                writeStringToFile(matchesForPaper, "jmo_paper_" + color + ".csv");
+
+                String sMatches = showMatchesForSpreadsheet(con, true, color);
+                writeStringToFile(sMatches, "jmo_spreadsheet_" + color + ".csv");
+            }
+
+            con.close();
+        }
+        catch (SQLException e) {
+            System.err.printf("Error getting data from: %s%n", url);
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeStringToFile(String sMatchList, String sFile){
+        try {
+            BufferedWriter bfwMatchList = new BufferedWriter(new FileWriter(sFile));
+            bfwMatchList.write(sMatchList);
+            bfwMatchList.close();
+        }
+        catch (IOException ioException) {
+            System.err.printf("Could not write file %s !%n", sFile);
+            ioException.printStackTrace();
+        }
+    }
+
+    public static String showMatchesForSpreadsheet(Connection con, Boolean makeTable, String color) throws SQLException {
+        StringBuilder sbResult = new StringBuilder();
         String selectTeamNumbers = "SELECT DISTINCT number FROM teams ORDER BY number";
         ArrayList<Integer> teamList = new ArrayList<>();
         Statement stmt = con.createStatement();
@@ -16,36 +77,85 @@ public class JudgeHelper {
         while (rsTeamList.next()) {
             teamList.add(rsTeamList.getInt("number"));
         }
+        sbResult.append(makeTable ? header_jmo : header_pit);
 
-        if (makeTable) {
-            System.out.println(header);
-        }
         for (Integer teamNumber: teamList) {
-            String selectMatches = String.format(
+            String selectAllMatches = String.format(
                     "SELECT match, (SELECT TeamNameShort FROM Team WHERE TeamNumber = %d) " +
                             "FROM quals WHERE %d IN (red1, red2, blue1, blue2) ORDER BY match;",
                     teamNumber, teamNumber);
-            ResultSet rsGetMatches = stmt.executeQuery(selectMatches);
+            String selectRedMatches = String.format(
+                    "SELECT match, (SELECT TeamNameShort FROM Team WHERE TeamNumber = %d) " +
+                            "FROM quals WHERE %d IN (red1, red2) ORDER BY match;",
+                    teamNumber, teamNumber);
+            String selectBlueMatches = String.format(
+                    "SELECT match, (SELECT TeamNameShort FROM Team WHERE TeamNumber = %d) " +
+                            "FROM quals WHERE %d IN (blue1, blue2) ORDER BY match;",
+                    teamNumber, teamNumber);
+
+            ResultSet rsGetMatches;
+            if (color.equalsIgnoreCase(both)) {
+                rsGetMatches = stmt.executeQuery(selectAllMatches);
+
+            } else if (color.equalsIgnoreCase(red)) {
+                rsGetMatches = stmt.executeQuery(selectRedMatches);
+
+            } else if (color.equalsIgnoreCase(blue)) {
+                rsGetMatches = stmt.executeQuery(selectBlueMatches);
+            }
+            else {
+                throw new IllegalArgumentException("Bad color");
+            }
 
             if (makeTable) {
                 while (rsGetMatches.next()) {
-                    System.out.printf("%d \t %d %n", teamNumber, rsTeamList.getInt("match"));
+                    sbResult.append(String.format("%d \t %d %n", teamNumber, rsTeamList.getInt("match")));
                 }
             }
             else
             // Team-finder mode: print the matches the teams are in
             {
-                System.out.printf("%d \t %s \t ", teamNumber, rsTeamList.getString(2));
+                sbResult.append(String.format("%d \t %s \t ", teamNumber, rsTeamList.getString(2)));
                 StringBuilder sb = new StringBuilder();
                 while (rsGetMatches.next()) {
                     sb.append(String.format("%d \t ", rsTeamList.getInt("match")));
                 }
-                System.out.printf("%s %n", sb.toString().replaceAll("\\s+$",""));
+                sbResult.append(String.format("%s %n", sb.toString().replaceAll("\\s+$","")));
             }
         }
+        return  sbResult.toString();
     }
 
-
+    /** This shows the match list for the JMOs to use on paper. */
+    public static String showMatchesForPaper(Connection con, String color) throws SQLException {
+        StringBuilder sb = new StringBuilder();
+        sb.append(header_jmo);
+        int currentMatch = 0;
+        String query = "SELECT match, red1, red2, blue1, blue2 FROM quals ORDER BY match";
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        while (rs.next()) {
+            currentMatch++;
+            int match = rs.getInt("match");
+            int red1 = rs.getInt("red1");
+            int red2 = rs.getInt("red2");
+            int blue1 = rs.getInt("blue1");
+            int blue2 = rs.getInt("blue2");
+            if (color.equalsIgnoreCase(both)) {
+                sb.append(match).append("\t").append(red1).append(System.lineSeparator());
+                sb.append(match).append("\t").append(red2).append(System.lineSeparator());
+                sb.append(match).append("\t").append(blue1).append(System.lineSeparator());
+                sb.append(match).append("\t").append(blue2).append(System.lineSeparator());
+            } else if (color.equalsIgnoreCase(red)) {
+                sb.append(match).append("\t").append(red1).append(System.lineSeparator());
+                sb.append(match).append("\t").append(red2).append(System.lineSeparator());
+            } else if (color.equalsIgnoreCase(blue)) {
+                sb.append(match).append("\t").append(blue1).append(System.lineSeparator());
+                sb.append(match).append("\t").append(blue2).append(System.lineSeparator());
+            }
+        }
+        return sb.toString();
+    }
     /** This shows the match list in its usual form. */
     public static void showMatchListRegular(Connection con) throws SQLException {
         String query = "SELECT match, red1, red2, blue1, blue2 FROM quals ORDER BY match";
@@ -62,21 +172,4 @@ public class JudgeHelper {
         }
     }
 
-    public static void main(String[] args) {
-
-        try {
-            Connection con = DriverManager.getConnection(url);
-
-            // showMatchListRegular(con);
-            showMatches(con, true);
-            System.out.printf("%n%n");
-            showMatches(con, false);
-
-            con.close();
-        }
-        catch (SQLException e) {
-            System.err.printf("Error getting data from: %s%n", url);
-            e.printStackTrace();
-        }
-    }
 }
