42d642c45480bca4cf30697b03d2cf682e371ae7
JohnMMcD <JohnMMcD@users.noreply.github.com>
2025-03-07 22:09:01 -0500

    Update JudgeMatchHelper.js to use API
    There is still a bit of HTML scraping in order to preserve some elements of the page.

--- JudgeMatchHelper.js
+++ JudgeMatchHelper.js
@@ -1,50 +1,97 @@
-// Purpose: to save the judges' time when they are trying to find the best time to interview a team. Typically, 
-// the best time is when a team has a break between matches. This script lists all the matches that a team is in, 
-// based on the Match Schedule. 
+// On a computer (desktop or laptop, not an iOS / Android device), connect to the
+// scoring system and open the Event Info page for the current event. You do not need to authenticate.
+// Open the Match Results Page. The URL should look like:
+// http://SCORING_SYSTEM_IP_ADDRESS_HERE/event/YOUR_EVENT_CODE_HERE/results/
+// Open the browser's DevTools - Ctrl-Shift-I on Windows or Option-Command-I on Mac.
+// Select the Console tab, if it's not already selected.
+// Copy the script below, and then paste the script into the Console.
+// The first time you paste in the script, you will
+// receive a scary warning about pasting in code from unknown sources. You will have to
+ // type something in the console window (usually "allow pasting")
+ // to indicate you know what you are doing.
 
-// Instructions: On a computer (desktop or laptop, not an iOS / Android device), connect to the
-// scoring system and open the Match Schedule. The URL should look like:
-//   http://SCORING_SYSTEM_IP_ADDRESS_HERE/event/YOUR_EVENT_CODE_HERE/schedule/
-// Open the browser's DevTools.
-// Copy the script below, and then paste the script into the console. The first time you do this, you will
-// receive a scary warning about pasting in code from unknown sources. You will have to type something in
-// the console window to indicate you know what you are doing.
-// If you have copied and pasted all the lines, you should see a list of teams and the matches they 
-// play in. Copy and paste the list into Notepad to print it out.
+const showField = true; // set to false if you don't want to see the field numver
 
 function getElementByXpath(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-// This map's key is the team number, and the value is an array of matches the team is playing in.
-var mapMatches = new Map();
-
-var table = getElementByXpath("(//table)[2]/tbody");
-for (var i = 0, row; row = table.rows[i]; i++) {
-   for (var j = 3, col; col = row.cells[j]; j++) {
-      // Each cell holds the team number for a match, with a * at the end if the match is a surrogate.
-      // Get the team number without the surrogate designator
-      var team = col.textContent.replaceAll("*", "");
-      // Get the array of the team's matches (if any)
-      var matches = (mapMatches.get(team) === undefined) ? [] : mapMatches.get(team);
-
-      // Prepare the surrogate designator
-      var isSurrogate = (col.textContent.indexOf("*") > 0) ? "*": "";
-      // Add the current match number to the array
-      matches.push(row.cells[1].textContent.replaceAll("Qualification ", "") + isSurrogate);
-      // Update the map with the new array
-      mapMatches.set(team, matches);
-   }
+function clearTable() {
+  const elem = getElementByXpath("//table");
+  elem.remove();
 }
 
-mapSorted = new Map([...mapMatches.entries()].sort( (a, b) => parseInt(a) > parseInt(b) ? 1 : -1 ));
+function populateHeader(obj) {
+  const matches = obj.matches;
+  var mapMatches = new Map();
+  for (const match of matches) {
+    const currentMatchNumber = match.matchNumber;
+    const fieldNum = match.field;
+     const teams = new Array(match.red.team1, match.red.team2, match.blue.team1, match.blue.team2);
+     for (const team of teams) {
+         var matchesByTeam = (mapMatches.get(team) === undefined) ? [] : mapMatches.get(team);
+         const data = currentMatchNumber + (showField ? "F" + fieldNum : "");
+         // Add the current match number to the array
+         matchesByTeam.push(data);
+         // Update the map with the new array
+         mapMatches.set(team, matchesByTeam);
+     } // end loop over teams in a match
+  } // end loop over matches
+  // Sort the matches, then show them
+  const mapSorted = new Map([...mapMatches.entries()].sort( (a, b) => parseInt(a) > parseInt(b) ? 1 : -1 ));
+  writeMatches(mapSorted);
+} // end populateHeader
 
-var output = '\n';
-function logMapElements(value, key, map) {
-  output = output + `Team ${key} matches: ${value}` + '\n';
+function writeMatches(mapTeams) {
+  const header = document.querySelector("small.col-12.text-center");
+  var pageHeader = document.querySelector("h2.text-center.col-5");
+  pageHeader.textContent = "Team Finder";
+  var tb = document.createElement("table");
+  var tbHeaderRow = document.createElement("tr");
+  tbHeaderRow.style.border = '2px solid blue';
+  var tbHeaderTeamNum = document.createElement("th");
+  tbHeaderTeamNum.textContent = "Team Number";
+  tbHeaderTeamNum.style.border = '1px solid red';
+  var tbHeaderMatches = document.createElement("th");
+  tbHeaderMatches.style.border = '1px solid red';
+  tbHeaderMatches.textContent = "Matches";
+  tbHeaderMatches.colSpan = "5";
+  tbHeaderMatches.style.textAlign = "center";
+
+  tbHeaderRow.appendChild(tbHeaderTeamNum);
+  tbHeaderRow.appendChild(tbHeaderMatches);
+  tb.appendChild(tbHeaderRow);
+
+  for (const team of mapTeams) {
+      var row = document.createElement("tr");
+      row.style.borderBottom = '2px solid orange';
+      const matchesPlayed = team[1];
+      var teamNum = document.createElement("td");
+      teamNum.style.border = '1px dotted orange';
+      teamNum.textContent = team[0];
+      row.appendChild(teamNum);
+      for (const m of matchesPlayed) {
+        var col = document.createElement("td");
+        col.style.border = '1px dotted green';
+        col.style.paddingLeft = '10px';
+        col.style.paddingRight = '10px';
+        col.textContent = m;
+        row.appendChild(col);
+      }
+      tb.appendChild(row);
+  }
+  header.appendChild(tb);
 }
-mapSorted.forEach(logMapElements);
 
-console.log(output);
+async function populate() {
+  const path = location.pathname;
+  const eventCode = path.replace("/event/", "").replace("/results/", "");
+  const requestURL = `http://${window.location.hostname}/api/v1/events/${eventCode}/matches/`;
+  const request = new Request(requestURL);
+  const response = await fetch(request);
+  const matchDetails = await response.json();
+  clearTable();
+  populateHeader(matchDetails);
+}
 
-
+await populate();
